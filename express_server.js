3172ea58836e807f244482e062857aab4e968e8a
promilo <milind.shah@mail.utoronto.ca>
2017-05-10 19:22:52 +0000

    Implemented cookie functionality for login

--- express_server.js
+++ express_server.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
-
+var cookieParser = require('cookie-parser')
 app.set("view engine", "ejs")
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 
 
@@ -15,9 +16,9 @@ var urlDatabase = {
   // "shortURL": "longURL"
 };
 
-// app.get("/", (req, res) => {
-//   res.end("Hello!");
-// });
+app.get('/', (req, res) => {
+  res.redirect('/urls');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -35,7 +36,8 @@ app.get("/hello", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase,
+    username : req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -72,17 +74,27 @@ app.get("/u/:shortURL", (req, res) => {
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
-})
+});
 
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { urls: urlDatabase,
                        shortURL: req.params.shortURL};
   urlDatabase[req.params.shortURL] = req.body.updateLongURL;
   res.render("urls_show", templateVars);
-})
+});
 
 app.post ('/urls/:shortURL/update', (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.updateLongURL;
 
   res.redirect('/urls');
-})
+});
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
+});
